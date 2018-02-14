1e3812f506fa7cf5c5d6bc76c8d445d9d37a8028
Dani Castilho <danicastilhodev@gmail.com>
2018-02-14 00:50:24 -0200

    start a booklist app

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
+import BookList from '../containers/book-list'
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <BookList />
     );
   }
 }
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'one' },
+        {title: 'two'},
+        {title: 'three'},
+        {title: 'four'},
+        {title: 'five'},
+    ]
+}
