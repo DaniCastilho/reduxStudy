7349e5b4d68a440c779f64053e8f4c73f9ce7e21
Daniele Ligieri Castilho <daniele.castilho@easynvest.com.br>
2018-02-19 13:51:52 -0300

    Revendo sessao de redux

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
     return [
-        {title: 'one' },
-        {title: 'two'},
-        {title: 'three'},
-        {title: 'four'},
-        {title: 'five'},
+        { title: 'Book 1'},
+        { title: 'Book 2'},
+        { title: 'Book 3'},
+        { title: 'Book 4'},
+        { title: 'Book 5'},
     ]
 }
